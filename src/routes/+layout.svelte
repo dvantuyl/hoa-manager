ec03697badde88c162cf159aa5f873e64689dbce
Dwight VanTuyl <dvantuyl@gmail.com>
2023-08-28 22:43:42 -0700

    Setup tailwindcss

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { firestore, auth, storage } from '$firebase';
 	import { FirebaseApp } from 'sveltefire';
+	import '../app.css';
 </script>
 
 <FirebaseApp {auth} {firestore} {storage}>
